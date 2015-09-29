d5bce647f783ede09aaa2b537ecb422b5e98746f
Tim Van Steenburgh <tvansteenburgh@gmail.com>
2015-09-29 18:12:03 -0400

    Pass in environment; better cleanup; clone JUJU_HOME

--- build-cloud.sh
+++ build-cloud.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
+
+export ENV=$1
+
 bash <<"EOT"
 set -x
 
+function cleanup {
+  # Clean up by destroying the environment.
+  juju destroy-environment --yes --force $ENV || true
+
+  # Clean up temp files/dirs
+  rm -rf $JUJU_REPOSITORY
+  rm -rf $testdir
+  rm -f $LOG_DEST
+  sudo rm -rf $TMP_JUJU_HOME
+  sudo rm -rf $TMP
+}
+trap cleanup EXIT
+
+# Create a new, temporary JUJU_HOME from the real one
+export REAL_JUJU_HOME=$HOME/cloud-city
+export TMP_JUJU_HOME=$(mktemp -d)
+sudo cp -R $REAL_JUJU_HOME/* $TMP_JUJU_HOME
+rm -rf $TMP_JUJU_HOME/environments/*
+
 export HERE=$(pwd)
-export ENV=charm-testing-aws
-export JUJU_HOME=$HOME/cloud-city
 export JUJU_REPOSITORY=$(mktemp -d)
 export LOG_DEST=$(mktemp)
 export TMP=$(mktemp -d)
@@ -27,7 +47,7 @@ export JUJU_VERSION=$(juju status -e $ENV | grep agent-version | head -n1 | tr -
 export START=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 
 mkdir -m 700 ${TMP}/ssh
-cp /var/lib/jenkins/cloud-city/staging-juju-rsa ${TMP}/ssh/id_rsa
+cp $TMP_JUJU_HOME/staging-juju-rsa ${TMP}/ssh/id_rsa
 
 sudo docker pull jujusolutions/charmbox:latest
 sudo docker run --rm \
@@ -35,7 +55,7 @@ sudo docker run --rm \
     -e "HOME=/home/ubuntu" \
     -e "JUJU_HOME=/home/ubuntu/.juju" \
     -w "/home/ubuntu" \
-    -v ${JUJU_HOME}:/home/ubuntu/.juju \
+    -v ${TMP_JUJU_HOME}:/home/ubuntu/.juju \
     -v ${TMP}/.deployer-store-cache:/home/ubuntu/.juju/.deployer-store-cache \
     -v ${JUJU_REPOSITORY}:/home/ubuntu/charm-repo \
     -v ${TMP}:${TMP} \
@@ -83,7 +103,7 @@ testdir=${artifacts[0]}
 bundlefile=${artifacts[1]}
 
 # upload results.json
-s3cmd -c ~/cloud-city/juju-qa.s3cfg put $OUTPUT s3://juju-qa-data/charm-test/${JOB_NAME}-${BUILD_NUMBER}-results.json
+s3cmd -c $TMP_JUJU_HOME/juju-qa.s3cfg put $OUTPUT s3://juju-qa-data/charm-test/${JOB_NAME}-${BUILD_NUMBER}-results.json
 
 # get and upload all-machines.log
 bash <<LOGPERMS
@@ -92,21 +112,14 @@ LOGPERMS
 timeout 1m juju scp -e $ENV 0:/var/log/juju/all-machines.log $LOG_DEST
 if [ -s $LOG_DEST ]; then
     tail $LOG_DEST
-    s3cmd -c ~/cloud-city/juju-qa.s3cfg put $LOG_DEST s3://juju-qa-data/charm-test/${JOB_NAME}-${BUILD_NUMBER}-all-machines-log
-    rm -f $LOG_DEST
+    s3cmd -c $TMP_JUJU_HOME/juju-qa.s3cfg put $LOG_DEST s3://juju-qa-data/charm-test/${JOB_NAME}-${BUILD_NUMBER}-all-machines-log
 fi
 
 if [ -n "${bundlefile}" ]; then
-    s3cmd -c ~/cloud-city/juju-qa.s3cfg put ${bundlefile} s3://juju-qa-data/charm-test/${JOB_NAME}-${BUILD_NUMBER}-bundle
+    s3cmd -c $TMP_JUJU_HOME/juju-qa.s3cfg put ${bundlefile} s3://juju-qa-data/charm-test/${JOB_NAME}-${BUILD_NUMBER}-bundle
 fi
 
-# Clean up by destroying the environment.
-juju destroy-environment --yes --force $ENV || true
-
 cp $OUTPUT $HERE
-rm -rf $JUJU_REPOSITORY
-rm -rf $testdir
-sudo rm -rf $TMP
 
 exit $EXIT_STATUS
 EOT
@@ -128,7 +141,7 @@ fi
 
 console_output=$(mktemp)
 curl -s --output $console_output ${BUILD_URL}consoleText
-s3cmd -c ~/cloud-city/juju-qa.s3cfg put $console_output s3://juju-qa-data/charm-test/${JOB_NAME}-${BUILD_NUMBER}-consoleText
+s3cmd -c $JUJU_HOME/juju-qa.s3cfg put $console_output s3://juju-qa-data/charm-test/${JOB_NAME}-${BUILD_NUMBER}-consoleText
 rm -rf $console_output
 
 exit $EXIT_STATUS
